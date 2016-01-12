5c23f11ecb92f5fd4728945142838f4c6fd1aaac
LukeBevDev <LukeBevDev@outlook.com>
2016-01-12 23:34:36 +0000

    Creation of webapp server for serving html and agnularjs powered web pages.

---
+++ AngularJsWebApp/src/main/java/com/bevan/projectenhance/angularjswebapp/WebAppServer.java
@@ -0,0 +1,39 @@
+package com.bevan.projectenhance.angularjswebapp;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by Luke on 12-Jan-16.
+ */
+public class WebAppServer {
+
+    private static final Logger LOGGER = Logger.getLogger(WebAppServer.class.getName());
+
+    private static final int PORT = 8080;
+    private static final String webAppDir = "src/main/webapp/";
+
+    public static void main(String[] args) {
+        Server server = new Server(PORT);
+
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setContextPath("/");
+        webAppContext.setDescriptor(webAppDir + "/WEB-INF/web.xml");
+        webAppContext.setResourceBase(webAppDir);
+
+        server.setHandler(webAppContext);
+
+        try {
+            LOGGER.info("Starting server.....");
+            server.start();
+            server.join();
+        } catch(Exception e) {
+            LOGGER.severe("Exception thrown in RestServer");
+            LOGGER.severe("Stacktrace: " + e.getStackTrace());
+        } finally {
+            server.destroy();
+        }
+    }
+}
